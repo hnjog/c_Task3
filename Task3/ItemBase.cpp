e4f352cc9e6c29083e5ea36592242953afaf4309
Hyun <hnjogo@gmail.com>
2025-08-21 19:31:30 +0900

    make item class

---
+++ Task3/ItemBase.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "ItemBase.h"
+
+ItemBase::ItemBase(string name, int price)
+	:name_(name),
+	price_(price)
+{
+}
+
+ItemBase::ItemBase(const ItemBase& other)
+{
+	name_ = other.name_;
+	price_ = other.price_;
+}
+
+ItemBase::~ItemBase()
+{
+	name_.clear();
+}
+
+void ItemBase::PrintInfo() const
+{
+	cout << "[이름: " << name_ << ", 가격: " << price_ << "G]" << '\n';
+}
---
+++ Task3/ItemBase.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include<string>
+
+using namespace std;
+
+class ItemBase
+{
+public:
+	ItemBase(string name, int price);
+	ItemBase(const ItemBase& other);
+	virtual ~ItemBase();
+
+	void PrintInfo() const;
+
+protected:
+	string name_;
+	int price_;
+};
